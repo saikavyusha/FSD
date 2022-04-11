a4c4a1d33e88db4a1d9141137436e10836684d49
saikavyusha <saikavyushaponnaganti@gmail.com>
2022-04-11 14:57:14 +0530

    Full Stack Development

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log(true + 100 +'300' - false);
+console.log(700+'600'/0);
+console.log('name'+'age'+false+null);
+console.log(100+200+'700'-'300'/0);
+console.log('500'-'700'/'string'+false);
